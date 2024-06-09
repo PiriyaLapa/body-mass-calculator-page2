40a636c36bd7e22cd3b115c9c1935dc05a1608ef
PiriyaLapa <piriyalapa@gmail.com>
2024-06-09 15:48:33 +0700

    update and try to deploy

--- src/components/forms/MetricForm.tsx
+++ src/components/forms/MetricForm.tsx
@@ -1,4 +1,4 @@
-import { Component, ReactNode } from "react";
+import { ChangeEvent, Component, KeyboardEvent, ReactNode } from "react";
 import { StyledMetricForm } from "../../styles/styles";
 import ResultBox from "./ResultBox";
 import { BmiFormProps } from "./BmiForm";
@@ -20,15 +20,33 @@ export default class MetricForm extends Component<
     };
   }
 
+  handleChangeInput = (event:ChangeEvent<HTMLInputElement>) => {
+      this.setState({
+        height:event.target.value,
+        weight:event.target.value
+      }) 
+  }
+
+ handleKeyPress = (event:KeyboardEvent<HTMLInputElement>) => {
+    if(event.key === "Enter"){
+      this.logValue();
+    }
+  }
+
+  logValue = () => {
+    console.log('Height:',this.state.height);
+    console.log('Weight:',this.state.weight);
+  }
+
   render(): ReactNode {
     const { height, weight } = this.props.bmiContent;
     return (
       <StyledMetricForm>
         <h2>Metric From</h2>
         <label htmlFor="height">{height}</label>
-        <input type="text" name="height" id="height" value={height} />
+        <input type="text" name="height" id="height" value={height} onChange={this.handleChangeInput} onKeyDown={this.handleKeyPress}/>
         <label htmlFor="weight">{weight}</label>
-        <input type="text" name="weight" id="weight" value={weight} />
+        <input type="text" name="weight" id="weight" value={weight} onChange={this.handleChangeInput} onKeyDown={this.handleKeyPress}/>
         <ResultBox />
       </StyledMetricForm>
     );
