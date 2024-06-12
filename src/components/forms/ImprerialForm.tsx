353adeed3960cb1cb8969fd0d10fed1a5879a96b
Piriya <piriyalapa@gmail.com>
2024-06-12 23:43:56 +0700

    update and try to deployo

--- src/components/forms/ImprerialForm.tsx
+++ src/components/forms/ImprerialForm.tsx
@@ -1,18 +1,51 @@
-import { Component, ReactNode } from "react";
+import { ChangeEvent, Component, ReactNode,KeyboardEvent } from "react";
 import { StyledImprerialForm } from "../../styles/styles";
 import ResultBox from "./ResultBox";
 import { BmiFormProps } from "./BmiForm";
 
-export default class ImprerialForm extends Component<BmiFormProps> {
+interface ImprerialState{
+  height:string;
+  weight:string;
+  
+}
+
+export default class ImprerialForm extends Component<BmiFormProps,ImprerialState> {
+  constructor(props: BmiFormProps) {
+    super(props);
+    this.state = {
+      height:'',
+      weight:''
+    }
+  }
+  
+  handleChangeInput = (event:ChangeEvent<HTMLInputElement>) => {
+    const {name,value} = event.target;
+    this.setState({
+      [name]:value
+    }as unknown as ImprerialState  )
+  }
+  
+  handleKeyPress = (event:KeyboardEvent<HTMLInputElement>) => {
+    if(event.key === "Enter"){
+      event.preventDefault();
+      this.logValue();
+    } 
+  }
+
+  logValue = () =>{
+    console.log("Height", this.state.height);
+    console.log("Weight", this.state.weight);
+  }
+
   render(): ReactNode {
-    const { height, weight } = this.props.bmiContent;
+    const { height, weight } = this.state;
     return (
       <StyledImprerialForm>
-      <h2>Imperial Form</h2>
-        <label htmlFor="height">{height} (in inches):</label>
-        <input type="text" name="height" id="height" />
-        <label htmlFor="weight">{weight}(in pounds):</label>
-        <input type="text" name="weight" id="weight" />
+        <h2>Imperial Form</h2>
+        <label htmlFor="height">height (in inches):</label>
+        <input type="text" name="height" id="height" value={height} onChange={this.handleChangeInput}  onKeyDown={this.handleKeyPress} />
+        <label htmlFor="weight">weight(in pounds):</label>
+        <input type="text" name="weight" id="weight" value={weight} onChange={this.handleChangeInput} onKeyDown={this.handleKeyPress}/>
         <ResultBox />
       </StyledImprerialForm>
     );
