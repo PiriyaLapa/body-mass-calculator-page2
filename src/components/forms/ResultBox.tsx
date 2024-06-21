798cfcfb4b46b497606f14155a2a3121f8a51371
Piriya <piriyalapa@gmail.com>
2024-06-21 21:35:32 +0700

    update and try to deploy

--- src/components/forms/ResultBox.tsx
+++ src/components/forms/ResultBox.tsx
@@ -8,12 +8,15 @@ interface resultProps {
   };
 }
 export default class ResultBox extends Component<resultProps> {
-  BmiCal = (height, weight) =>{
-      return console.log(height,weight); 
+  CalBmi(weight: number, height: number): number {
+    return weight / Math.pow(height, 2);
   }
-  
+
   render(): ReactNode {
     const { height, weight } = this.props.state;
+    const bmiResult = this.CalBmi(parseInt(weight), parseInt(height));
+    // height , weight define to cm
+
     return (
       <StyledResultBox>
         <span>
@@ -21,9 +24,10 @@ export default class ResultBox extends Component<resultProps> {
           ea cupiditate reiciendis sunt quae neque quidem eos ipsum cumque
           nesciunt libero impedit repudiandae provident, doloribus fuga commodi
           autem eum!
-        </span>
+        </span>{" "}
         <p>height : {height}</p>
         <p>weight: {weight}</p>
+        <h2>{bmiResult}</h2>
       </StyledResultBox>
     );
   }
